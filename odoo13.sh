4e4c70dceb0902c91704aed41c6519589fd355ea
ansinaesmejor <ansinaesmejor@gmail.com>
2020-05-06 08:38:10 -0500

    Implementar odoo 13.0

--- odoo13.sh
+++ odoo13.sh
@@ -6,6 +6,8 @@
 
 domain=ansinaesmejor.com
 email=ansinaesmejor@gmail.com
+# VARIABLE MAX MEMORY PERCENT
+CMD_M=80
 
 OS_NAME=$(lsb_release -cs)
 usuario=$USER
@@ -88,36 +90,15 @@ sudo pip3 install -r $PATHBASE/$VERSION/odoo/requirements.txt
 sudo apt-get -f -y install
 
 # worker_analizer
-
 # CONST 1GB
-
 CONST_1GB="1024*1024*1024"
-
-# VARIABLE WORKERS
-
-CMD_W=0
-
-# VARIABLE MAX MEMORY PERCENT
-
-CMD_M=80
-
-# VARIABLE IS HELP
-
-CMD_H=0
-
-# VARIABLE IS VERBOSE
-
-CMD_V=0
-
-# FUNCTIONS
 
 arithmetic() {
   echo "scale=0; $1" | bc
 }
 
 calculateWorkers(){
-  if [ $CMD_W -gt 0 ]; then echo $CMD_W
-  elif [ $(calculateMaxMemory) -le $(arithmetic "$CONST_1GB") ]; then echo 1 # 1GB
+  if [ $(calculateMaxMemory) -le $(arithmetic "$CONST_1GB") ]; then echo 1 # 1GB
   elif [ $(calculateMaxMemory) -le $(arithmetic "2*$CONST_1GB") ]; then echo 2 # 2GB
   elif [ $(calculateMaxMemory) -le $(arithmetic "3*$CONST_1GB") ]; then echo 3 # 3GB
   else
@@ -145,57 +126,13 @@ calculateLimitMemorySoft() {
   echo $(arithmetic "$(calculateLimitMemoryHard)*80/100")
 }
 
-# COMMANDS
-
-v() {
-  echo
-  echo "System Information"
-  echo "------------------"
-  echo "Cores (CORES):  $(calculateNumCores)"
-  echo "Total Memory (TOTAL_M): $(calculateMemTotal) bytes"
-  echo "Max Allowed Memory (ALLOW_M): $(calculateMaxMemory) bytes"
-  echo "Max Allowed Memory Percent, default 80%: $CMD_M%"
-  echo
-  echo
-  echo "Functions to calculate configutarion"
-  echo "------------------------------------"
-  echo "workers = if not used -w then"
-  echo "               if ALLOW_M < 1GB then 1"
-  echo "               else ALLOW_M < 2GB then 2"
-  echo "               else ALLOW_M < 3GB then 3"
-  echo "               else 1+CORES*2"
-  echo "          else -w"
-  echo "limit_memory_hard = ALLOW_M / workers"
-  echo "limit_memory_soft = limit_memory_hard * 80%"
-  echo "limit_request = DEFAULT 8192"
-  echo "limit_time_cpu = DEFAULT 60"
-  echo "limit_time_real = DEFAULT 120"
-  echo "max_cron_threads = DEFAULT 2"
-  echo
-  echo
-  echo "Add to the odoo-server.conf"
-  echo "---------------------------"
-  c
-  echo
-}
-
-h() {
-  echo "This file enables us to optimally configure multithreading settings Odoo"
-  echo "   -h    Help"
-  echo "   -m    Max memory percent to use"
-  echo "   -v    Verbose"
-  echo "   -w    Set static workers number"
-}
-
-c() {
-  echo "workers = $(calculateWorkers)"
-  echo "limit_memory_hard = $(calculateLimitMemoryHard)"
-  echo "limit_memory_soft = $(calculateLimitMemorySoft)"
-  echo "limit_request = 8192"
-  echo "limit_time_cpu = 60"
-  echo "limit_time_real = 120"
-  echo "max_cron_threads = 2"
-}
+confworkers=$(calculateWorkers)
+conflimit_memory_hard=$(calculateLimitMemoryHard)
+conflimit_memory_soft=$(calculateLimitMemorySoft)
+conflimit_request=8192
+conflimit_time_cpu=60
+conflimit_time_real=120
+confmax_cron_threads=1
 
 cd $DIR_PATH
 
@@ -225,36 +162,16 @@ addons_path =
 xmlrpc_port = $PORT
 ;dbfilter = odoo13
 logrotate = True
-;limit_time_real = 1000
-;limit_time_cpu = 1000
+limit_time_real = $conflimit_time_real
+limit_time_cpu = $conflimit_time_cpu
 
-for ((i=1;i<=$#;i++))
-do
-  case "${!i}" in
-    '-w') ((i++))
-    CMD_W=${!i}
-    ;;
-    '-m') ((i++))
-    if [ ${!i} -gt 0 ] && [ ${!i} -lt 80 ]; then CMD_M=${!i}
-    fi
-    ;;
-    '-v')
-    CMD_V=1
-    ;;
-    '-h')
-    CMD_H=1
-    ;;
-    *)
-    # NOTHING
-    ;;
-  esac
-done
-
-
-if [ $CMD_H -eq 1 ]; then h
-elif [ $CMD_V -eq 1 ]; then v
-else c
-fi
+workers = $confworkers
+limit_memory_hard = $conflimit_memory_hard
+limit_memory_soft = $conflimit_memory_soft
+limit_request = $conflimit_request
+limit_time_cpu = $conflimit_time_cpu
+limit_time_real = $conflimit_time_real
+max_cron_threads = $confmax_cron_threads
 
 " | sudo tee --append /opt/config/odoo$VCODE.conf
 
