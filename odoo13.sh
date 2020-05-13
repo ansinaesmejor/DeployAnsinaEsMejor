0daf5b81db483251b206ba0c1bd72a1ec98b0d4b
ansinaesmejor <64254242+ansinaesmejor@users.noreply.github.com>
2020-05-12 22:09:14 -0500

    Update odoo13.sh

--- odoo13.sh
+++ odoo13.sh
@@ -253,7 +253,6 @@ sudo ln -s /etc/nginx/sites-available/$domain /etc/nginx/sites-enabled/
 sudo mkdir /etc/nginx/ssl
 sudo openssl dhparam -out /etc/nginx/ssl/dhp-2048.pem 2048
 
-
 echo "
 upstream odoo {
     server 127.0.0.1:$PORT;
@@ -262,42 +261,33 @@ upstream odoo {
 upstream openerp-im {
     server 127.0.0.1:8072 weight=1 fail_timeout=0;
 }
-
 server {
     listen 443 default;
     server_name $domain;
-
     client_max_body_size 200m;
     proxy_read_timeout 300000;
-
     access_log	/var/log/nginx/odoo.access.log;
     error_log	/var/log/nginx/odoo.error.log;
-
     ssl on;
     ssl_certificate	/etc/letsencrypt/live/$domain/fullchain.pem;
     ssl_certificate_key	/etc/letsencrypt/live/$domain/privkey.pem;
     keepalive_timeout	60;
-
     ssl_ciphers	HIGH:!aNULL!ADH:!MD5;
     ssl_protocols	TLSv1 TLSv1.1 TLSv1.2;
     ssl_prefer_server_ciphers on;
     ssl_dhparam /etc/nginx/ssl/dhp-2048.pem;
-
     proxy_buffers 16 64k;
     proxy_buffer_size 128k;
-
     location / {
         proxy_pass http://odoo;
         proxy_next_upstream error timeout invalid_header http_500 http_502 http_503 http_504;
         proxy_redirect off;
-
         proxy_set_header    Host \$host;
         proxy_set_header    X-Real-IP \$remote_addr;
         proxy_set_header    X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header    X-Forwarded-Host  \$host;
         proxy_set_header    X-Forwarded-Proto https;
     }
-
     location ~* /web/static/ {
         proxy_cache_valid 200 60m;
         proxy_buffering on;
@@ -313,12 +303,10 @@ server {
     gzip_min_length 1000;
     gzip_proxied    expired no-cache no-store private auth;
 }
-
 server {
     listen	80;
     server_name www.$domain $domain;
     listen [::]:80 ipv6only=on;
-
     add_header Strict-Transport-Security max-age=2592000;
     return 301 https://\$host\$request_uri;
 }" > /etc/nginx/sites-available/$domain
